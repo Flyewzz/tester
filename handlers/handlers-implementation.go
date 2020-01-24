1b3f280e62bd5485acd3c04f03004901b999a851
Flyewzz <dentalon599@gmail.com>
2020-01-25 02:05:47 +0300

    Added receive a code from a client

--- handlers/handlers-implementation.go
+++ handlers/handlers-implementation.go
@@ -1,16 +1,13 @@
 package handlers
 
 import (
-	"io"
-	"log"
+	"fmt"
 	"net/http"
 	"os"
 	"text/template"
 )
 
 func (api *ApiManager) MainHandler(w http.ResponseWriter, r *http.Request) {
-	// w.Write([]byte("Задание: Правильное сложение чисел"))
-
 	type Data struct {
 		Title string
 	}
@@ -18,46 +15,20 @@ func (api *ApiManager) MainHandler(w http.ResponseWriter, r *http.Request) {
 	title := "Сложите два числа"
 	t.Execute(w, Data{
 		Title: title,
-	}) // merge.
-	// method, url := r.URL.Query().Get("method"), r.URL.Query().Get("url")
-	// if !CheckMethodValid(method) {
-	// 	http.Error(w, "Bad request", http.StatusBadRequest)
-	// 	return
-	// }
-	// task := &models.Task{
-	// 	Method: method,
-	// 	Url:    url,
-	// }
-	// resCh := hd.Dispatcher.AddNewTask(task)
-	// result := <-resCh
-	// if result.Error != nil {
-	// 	http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
-	// 	log.Println(result.Error)
-	// 	return
-	// }
-	// data, err := json.Marshal(result.Response)
-	// if err != nil {
-	// 	http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
-	// 	log.Println(err)
-	// 	return
-	// }
-	// w.Write(data)
+	})
 }
 
 func (api *ApiManager) TaskCheckerHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	// r.ParseMultipartForm(5 * 1024 * 1024)
-	file, _, err := r.FormFile("file")
+	code := r.PostFormValue("code")
+	fmt.Println(code)
+	// copy example
+	prog, _ := os.Create("main.cpp")
+	defer prog.Close()
+	_, err := prog.WriteString(code)
 	if err != nil {
-		log.Println(err)
 		http.Error(w, "500 Internal Server Error", http.StatusInternalServerError)
 		return
 	}
-	defer file.Close()
-	// copy example
-	f, err := os.OpenFile("./downloaded", os.O_WRONLY|os.O_CREATE, 0666)
-	defer f.Close()
-	io.Copy(f, file)
-	// method, url := r.URL.Query().Get("method"), r.URL.Query().Get("url")
-
+	w.Write([]byte("OK"))
+	// io.Copy(f, file)
 }
